fc8e9706335f75e095aae1ec812af1001b5cd8ec
vivalalova <vivalalova0@gmail.com>
2020-11-07 19:12:49 +0800

    Initial Commit

---
+++ swiftui-practice/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  swiftui-practice
+//
+//  Created by lova on 2020/11/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-practice/swiftui_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_practiceApp.swift
+//  swiftui-practice
+//
+//  Created by lova on 2020/11/7.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
