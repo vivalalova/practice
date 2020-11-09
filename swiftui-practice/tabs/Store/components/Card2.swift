7143aa5f39d8a2237339a1c3dacec1aab91fdf58
vivalalova <vivalalova0@gmail.com>
2020-11-09 09:47:21 +0800

    ke

--- swiftui-practice/tabs/Store/components/Card2.swift
+++ swiftui-practice/tabs/Store/components/Card2.swift
@@ -8,18 +8,29 @@
 import SwiftUI
 
 struct Card2: View {
+    let height: CGFloat = 200
+    let height1: CGFloat = 100
+
     var body: some View {
         Button(action: {}, label: {
             VStack {
-                Image(systemName: "trash")
-                Text("Hello, world!")
-                    .lineLimit(1)
+                VStack(alignment: .leading) {
+                    Text("重磅更新").font(.caption)
+                    Text("明日之後-末日生存手遊").font(.title2).fontWeight(.medium).foregroundColor(.black)
+                    Text("突破讀取界線").font(.title3).fontWeight(.medium).foregroundColor(.gray)
+
+                }.frame(height: height1)
+
+
+                Image("kiwi-cutie")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: height - height1)
+                    .clipped()
+                    .cornerRadius(8)
             }
         })
-            .frame(width: 120, height: 100)
-            .foregroundColor(.white)
-            .background(Color.red)
-            .cornerRadius(16)
+            .frame(width: 300, height: height)
     }
 }
 
